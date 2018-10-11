5123b593acae3def1e39cbf198e9fbe57a153a51
MrBonzzo <31772882+MrBonzzo@users.noreply.github.com>
2018-10-12 00:10:12 +0300

    Add files via upload

---
+++ server/webserver.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+app = Flask(__name__)
+
+
+sensors = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", sensors=sensors)
+
+@app.route("/sensors/<sensor>")
+def catch():
+	if request.headers.get('User-Agent') == sensor:
+		who = sensor
+		time = str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
+		sensors.append([who, time])
+	return ''
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80)
